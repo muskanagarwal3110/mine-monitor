24d2f535f5ae7c7eeef3083d18dfba9e18a4e339
Muskan Agarwal <muskanagarwal3110@gmail.com>
2026-04-15 00:03:28 +0530

    fix: linting issues

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
-
